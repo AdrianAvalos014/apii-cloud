fa7cfa007fb5c4b8e79dff68182326a9e7a3a1c1
AdrianAvalos014 <iscaavalos055@accitesz.com>
2026-04-30 23:07:24 -0600

    Actualizar modelo Tarea completo

--- main.py
+++ main.py
@@ -1,23 +1,40 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+from typing import Optional
 
 app = FastAPI()
 
-# "base de datos" simple en memoria (para demo)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# "base de datos" simple en memoria
 tareas = []
 
 class Tarea(BaseModel):
+    id: int
     titulo: str
-    descripcion: str
+    descripcion: Optional[str] = ""
+    fechaLimite: Optional[str] = ""
+    prioridad: str = "Media"
+    completada: bool = False
+
 
 @app.get("/")
 def home():
     return {"mensaje": "API funcionando en Render"}
 
+
 @app.post("/tareas")
 def crear_tarea(tarea: Tarea):
-    tareas.append(tarea)
+    tareas.append(tarea.dict())
     return {"msg": "Tarea creada", "data": tarea}
+
 
 @app.get("/tareas")
 def obtener_tareas():
